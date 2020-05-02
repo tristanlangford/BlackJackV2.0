123216c4fef23a01f940a2b9410c223178ef2129
Tristan <tristanlangford@hotmail.com>
2020-05-02 15:43:06 +0000

    Refracted & added start menu

--- blackjack_V2.rb
+++ blackjack_V2.rb
@@ -48,18 +48,16 @@ def total(hand)  # method to calc current hand total
   hand_total # return total
 end
 
-def move(current_hand) # interative part of the game
-  hand = current_hand # take current hand and make it updatable
+def move # interative part of the game
   puts "Hit or Stick?"
   loop do # loop to ensure value is hit or stick
     input = gets.chomp # capture hit or stick
     if input.downcase == "hit" # check for hit in any format
-      new_card = random_card 
-      hand.push(new_card) # issue new card and add to hand
-      puts "You got a #{new_card}" # notify user of their new card
-      running_total = total(hand) # get new total of hand
+      @player_hand.push(random_card)
+      puts "You got a #{@player_hand.last}" # notify user of their new card
+      running_total = total(@player_hand) # get new total of hand
       break if running_total > 21 # check to see if bust
-      if hand.include?("Ace") && (running_total + 10) < 21 # Ace check
+      if @player_hand.include?("Ace") && (running_total + 10) < 21 # Ace check
         puts "You have #{running_total} or #{running_total + 10}"
       else      
         puts "You have #{running_total}"
@@ -71,65 +69,116 @@ def move(current_hand) # interative part of the game
       puts "Please enter Hit or Stick" # error message
     end
   end
-  hand # return the hand
 end
 
-def final(player, dealer) # method to deal with end game. take in hand scores
-  puts "Player has #{player}, dealer has #{dealer}" # tell the user what each player has
-  if player > dealer && dealer < 21 # player more than dealer
+def final(player_total, dealer_total) # method to deal with end game. take in hand scores
+  puts "Player has #{player_total}, dealer has #{dealer_total}" # tell the user what each player has
+  if player_total > dealer_total && dealer_total < 21 # player more than dealer
     puts "Player wins!"
-  elsif dealer > 21 # dealer bust
+  elsif dealer_total > 21 # dealer bust
     puts "Dealer bust. Player wins!"
-  elsif dealer > player # dealer more than player
+  elsif dealer_total > player_total # dealer more than player
     puts "Dealer wins"
   else
-    puts "Draw" # draw
+    puts "Draw"
   end
 end
 
-def dealer(dealers_hand) # method for dealers turn
-  dealers_final_hand = dealers_hand # take dealers first 2 cards and make new hand var
-  dealer_running_total = total(dealers_hand) # calculate current hand total
-  puts "Dealer has a #{dealers_final_hand[0]} & a #{dealers_final_hand[1]}" # reveal dealer cards
-  while dealer_running_total < 18 do # if over 17, dealer will stick
-    if dealers_final_hand.include?("Ace") && (dealer_running_total + 10).between?(18, 21) # Ace Check
+def dealer # method for dealers turn
+  puts "Dealer has a #{@dealer_hand[0]} & a #{@dealer_hand[1]}" # reveal dealer cards
+  while total(@dealer_hand) < 18 do # if over 17, dealer will stick
+    break if total(@dealer_hand) > total(@player_hand)
+    if @dealer_hand.include?("Ace") && (total(@dealer_hand) + 10).between?(18, 21) # Ace Check
       break
     else  
-      dealers_final_hand.push(random_card) # add new card to hand
-      puts "Dealer got a #{dealers_final_hand.last}" # tell player the dealers card
-      dealer_running_total = total(dealers_final_hand) # update dealers total
-      if dealers_final_hand.include?("Ace") && (dealer_running_total + 10) < 18 # Ace Check
-        puts "Dealer is on #{dealer_running_total} or #{dealer_running_total + 10}" 
+      @dealer_hand.push(random_card) # add new card to hand
+      puts "Dealer got a #{@dealer_hand.last}" # tell player the dealers card
+      if @dealer_hand.include?("Ace") && (total(@dealer_hand) + 10) < 18 # Ace Check
+        puts "Dealer is on #{total(@dealer_hand)} or #{total(@dealer_hand) + 10}" 
       else 
-        puts "Dealer is on #{dealer_running_total}"
+        puts "Dealer is on #{total(@dealer_hand)}"
       end
     end
   end
-  total(dealers_final_hand) # return hand value
+end
+
+def starting_hands
+  @player_hand.push(random_card) until @player_hand.length == 2 # deal first 2 player cards
+  puts "You're dealt a #{@player_hand[0]} and a #{@player_hand[1]}" # reveal first 2 cards
+  
+  puts "You have #{total(@player_hand)}" if @player_hand.include?("Ace") == false # Ace Check
+  puts "You have #{total(@player_hand)} or #{total(@player_hand) + 10}" if @player_hand.include?("Ace")
+  @dealer_hand.push(random_card) until @dealer_hand.length == 2 # deal first 2 dealer cards
+  puts "Dealer has a #{@dealer_hand[0]} and another card" # reveal 1 dealer card
 end
 
 def run_game # method to run game
   puts "Welcome to BlackJack" # intro
   
-  player_hand = [] # array for starting cards
-  dealer_hand = []
+  @player_hand = [] # array for starting cards
+  @dealer_hand = []
   
-  player_hand.push(random_card) while player_hand.length < 2 # deal first 2 player cards
-  puts "You're dealt a #{player_hand[0]} and a #{player_hand[1]}" # reveal first 2 cards
-  starting_total = total(player_hand)
-  puts "You have #{starting_total}" if player_hand.include?("Ace") == false # Ace Check
-  puts "You have #{starting_total} or #{starting_total + 10}" if player_hand.include?("Ace")
-  dealer_hand.push(random_card) while dealer_hand.length < 2 # deal first 2 dealer cards
-  puts "Dealer has a #{dealer_hand[0]} and another card" # reveal 1 dealer card
+  starting_hands
   
-  player_hand = move(player_hand) # initiante players moves
-  player_total = total(player_hand) # capture player score
-  player_total += 10 if player_hand.include?("Ace") && player_total + 10 < 22 # Ace Check
+  move # initiante players moves
+  player_final_total = total(@player_hand) # capture player score
+  player_final_total += 10 if @player_hand.include?("Ace") && player_final_total + 10 < 22 # Ace Check
   
-  puts "You are bust with #{player_total}" if player_total > 21  # check for player bust
- 
-  dealer_total = dealer(dealer_hand) # dealer only goes if player not bust
-  final(player_total, dealer_total) if player_total < 22 # run end game
+  if player_final_total > 21  # check for player bust
+    puts "You are bust with #{player_final_total}"
+  else
+    dealer
+    dealer_final_total = total(@dealer_hand)
+    dealer_final_total += 10 if @dealer_hand.include?("Ace") && dealer_final_total + 10 < 22 # Ace Check
+    final(player_final_total, dealer_final_total) if player_final_total < 22 # run end game
+  end
+  
+  play_again
 end
+
+def play_again
+  puts "Play again? (Y/N)"
+  input = STDIN.gets.chomp
+  loop do 
+    if input.upcase == "Y"
+      run_game
+    elsif input.upcase == "N"
+      break
+    else
+      puts "Error: Enter Y or N"
+      input = STDIN.gets.chomp
+    end
+  end
 end
-BlackJack.new.run_game # run class & game
+
+def start_menu_options
+  puts "1. New Game"
+  puts "2. Load Game"
+  puts "3. Save Game"
+  puts "9. Exit"
+end
+
+def start_menu_action
+  input = STDIN.gets.chomp
+  case input
+    when "1"
+      run_game
+    when "2"
+      puts "Feature coming soon!"
+    when "3"
+      puts "Feature coming soon!"
+    when "9"
+      exit
+  end
+end
+
+def play
+  loop do
+    start_menu_options
+    start_menu_action
+  end
+end
+
+end
+        
+BlackJack.new.play # run class & game
